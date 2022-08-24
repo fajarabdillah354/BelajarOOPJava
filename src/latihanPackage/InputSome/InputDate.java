e399af616938ead3b5d64218fbd2366cf3dffefe
fajar abdillah ahmad <fajardillah25@gmail.com>
2022-08-24 21:08:42 +0700

    created new package for study

---
+++ src/latihanPackage/InputSome/InputDate.java
@@ -0,0 +1,20 @@
+package src.latihanPackage.InputSome;
+
+import java.util.Scanner;
+
+public class InputDate {
+
+    public void getInput(){
+
+            Scanner input = new Scanner(System.in);
+            System.out.println("Masukan Id anda : ");
+            String x = input.nextLine();
+            System.out.println("Masukan password anda : ");
+            String y = input.nextLine();
+
+
+    }
+
+
+}
+
