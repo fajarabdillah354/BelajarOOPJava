ae212d39a7d47c2553b84f746abcc9f7d68f3a36
fajar abdillah ahmad <fajardillah25@gmail.com>
2022-08-31 21:25:31 +0700

    penambahan untuk harga mobil < 700000000,dan perbaika beberapa function

--- src/latihanPackage/latihanAnonymousClass/ShowroomApp.java
+++ src/latihanPackage/latihanAnonymousClass/ShowroomApp.java
@@ -14,12 +14,16 @@ FUNCTION UNTUK PEMBAYARAN AGYA
         if (bayar.equals("cash")){
             var price = 170_000_000;
             System.out.println("nominal yang anda harus bayar : "+price);
-            System.out.print("KETIK DEAL UNTUK MEMBELI : ");
+            System.out.print("KETIK DEAL UNTUK MEMBELI NO UNTUK KEMBALI: ");
             String beli = input.nextLine();
             if (beli.equals("DEAL")){
                 System.out.println("Selamat anda telah membeli mobil Agya");
             } else if (beli.equals("deal")) {
                 System.out.println("Selamat anda telah membeli mobil Agya");
+            } else if (beli.equals("no")) {
+                Toyota();
+            } else if (beli.equals("NO")) {
+                Toyota();
             } else{
                 System.out.println("terimakasih telah mengunjungi Showroom kami");
             }
@@ -53,10 +57,14 @@ FUNCTION UNTUK PEMBAYARAN AVANZA
         if (bayar.equals("cash")){
             var price = 167_000_000;
             System.out.println("nominal yang anda harus bayar : "+price);
-            System.out.print("KETIK DEAL UNTUK MEMBELI : ");
+            System.out.print("KETIK DEAL UNTUK MEMBELI NO UNTUK KEMBALI : ");
             String beli = input.nextLine();
             if (beli.equals("DEAL")){
                 System.out.println("Selamat anda telah membeli mobil Avanza");
+            } else if (beli.equals("no")) {
+                Toyota();
+            } else if (beli.equals("NO")) {
+                Toyota();
             } else if (beli.equals("deal")) {
                 System.out.println("Selamat anda telah membeli mobil Avanza");
             } else {
@@ -92,10 +100,14 @@ FUNCTION UNTUK PEMBAYARAN AYLA
         if (bayar.equals("cash")){
             var price = 150_000_000;
             System.out.println("nominal yang anda harus bayar : "+price);
-            System.out.print("KETIK DEAL UNTUK MEMBELI : ");
+            System.out.print("KETIK DEAL UNTUK MEMBELI NO UNTUK KEMBALI : ");
             String beli = input.nextLine();
             if (beli.equals("DEAL")){
                 System.out.println("Selamat anda telah membeli mobil Ayla");
+            } else if (beli.equals("no")) {
+                Toyota();
+            } else if (beli.equals("NO")) {
+                Toyota();
             } else if (beli.equals("deal")) {
                 System.out.println("Selamat anda telah membeli mobil Ayla");
             } else {
@@ -121,6 +133,7 @@ FUNCTION UNTUK PEMBAYARAN AYLA
 
 
 /*
+ ****** MOBIL DI ATAS 200 JUTA *******
 FUCNTION PEMBAYARAN RUSH
  */
 
@@ -131,12 +144,16 @@ FUCNTION PEMBAYARAN RUSH
         if (bayar.equals("cash")){
             var price = 261_000_000;
             System.out.println("nominal yang anda harus bayar : "+price);
-            System.out.print("KETIK DEAL UNTUK MEMBELI : ");
+            System.out.print("KETIK DEAL UNTUK MEMBELI NO UNTUK KEMBALI : ");
             String beli = input.nextLine();
             if (beli.equals("DEAL")){
                 System.out.println("Selamat anda telah membeli mobil Rush");
             } else if (beli.equals("deal")) {
                 System.out.println("Selamat anda telah membeli mobil Rush");
+            } else if (beli.equals("NO")) {
+                Toyota();
+            } else if (beli.equals("no")) {
+                Toyota();
             } else {
                 System.out.println("terimakasih telah mengunjungi Showroom kami");
             }
@@ -170,12 +187,16 @@ FUNCTION UNTUK PEMBAYARAN FREED
         if (bayar.equals("cash")){
             var price = 258_000_000;
             System.out.println("nominal yang anda harus bayar : "+price);
-            System.out.print("KETIK DEAL UNTUK MEMBELI : ");
+            System.out.print("KETIK DEAL UNTUK MEMBELI NO UNTUK KEMBALI : ");
             String beli = input.nextLine();
             if (beli.equals("DEAL")){
                 System.out.println("Selamat anda telah membeli mobil Freed");
             } else if (beli.equals("deal")) {
                 System.out.println("Selamat anda telah membeli mobil Freed");
+            } else if (beli.equals("NO")) {
+                Toyota();
+            } else if (beli.equals("no")) {
+                Toyota();
             } else {
                 System.out.println("terimakasih telah mengunjungi Showroom kami");
             }
@@ -209,12 +230,58 @@ FUNCTION PEMBAYARAN RAIZE
         if (bayar.equals("cash")){
             var price = 275_000_000;
             System.out.println("nominal yang anda harus bayar : "+price);
-            System.out.print("KETIK DEAL UNTUK MEMBELI : ");
+            System.out.print("KETIK DEAL UNTUK MEMBELI NO UNTUK KEMBALI : ");
             String beli = input.nextLine();
             if (beli.equals("DEAL")){
                 System.out.println("Selamat anda telah membeli mobil Raize");
             } else if (beli.equals("deal")) {
                 System.out.println("Selamat anda telah membeli mobil Raize");
+            } else if (beli.equals("NO")) {
+                Toyota();
+            } else if (beli.equals("no")) {
+                Toyota();
+            } else {
+                System.out.println("terimakasih telah mengunjungi Showroom kami");
+            }
+
+        } else if (bayar.equals("kredit")) {
+            String[] kredit = {
+                    "6 bulan",
+                    "12 bulan",
+                    "15 bulan",
+                    "20 bulan",
+                    "30 bulan"
+
+            };
+            System.out.println("==== berikut adalah rincian kredit ====");
+            for (var i = 0; i < kredit.length; i++) {
+                System.out.println((i+1)+". "+kredit[i]);
+            }
+        }
+        return "terimakasih  ";
+    }
+
+    /*
+    ****** MOBIL DI BAWAH 700 JUTA ******
+    FUNCTION UNTUK PEMBAYARAN FORTUNER
+     */
+    static String pembayaranFortuner(){
+        Scanner input =new Scanner(System.in);
+        System.out.print("silahkan pilih metode pembayaran (cash/kredit) : ");
+        String bayar = input.nextLine();
+        if (bayar.equals("cash")){
+            var price = 564_000_000;
+            System.out.println("nominal yang anda harus bayar : "+price);
+            System.out.print("KETIK DEAL UNTUK MEMBELI NO UNTUK KEMBALI : ");
+            String beli = input.nextLine();
+            if (beli.equals("DEAL")){
+                System.out.println("Selamat anda telah membeli mobil Fortuner");
+            } else if (beli.equals("deal")) {
+                System.out.println("Selamat anda telah membeli mobil Fortuner");
+            } else if (beli.equals("NO")) {
+                Toyota();
+            } else if (beli.equals("no")) {
+                Toyota();
             } else {
                 System.out.println("terimakasih telah mengunjungi Showroom kami");
             }
@@ -238,6 +305,89 @@ FUNCTION PEMBAYARAN RAIZE
 
 
     /*
+    FUNCTION PEMBAYARAN VOXY
+     */
+
+    static String pembayaranVoxy(){
+        Scanner input =new Scanner(System.in);
+        System.out.print("silahkan pilih metode pembayaran (cash/kredit) : ");
+        String bayar = input.nextLine();
+        if (bayar.equals("cash")){
+            var price = 575_000_000;
+            System.out.println("nominal yang anda harus bayar : "+price);
+            System.out.print("KETIK DEAL UNTUK MEMBELI NO UNTUK KEMBALI : ");
+            String beli = input.nextLine();
+            if (beli.equals("DEAL")){
+                System.out.println("Selamat anda telah membeli mobil Voxy");
+            } else if (beli.equals("deal")) {
+                System.out.println("Selamat anda telah membeli mobil Voxy");
+            } else if (beli.equals("NO")) {
+                Toyota();
+            } else if (beli.equals("no")) {
+                Toyota();
+            } else {
+                System.out.println("terimakasih telah mengunjungi Showroom kami");
+            }
+
+        } else if (bayar.equals("kredit")) {
+            String[] kredit = {
+                    "6 bulan",
+                    "12 bulan",
+                    "15 bulan",
+                    "20 bulan",
+                    "30 bulan"
+
+            };
+            System.out.println("==== berikut adalah rincian kredit ====");
+            for (var i = 0; i < kredit.length; i++) {
+                System.out.println((i+1)+". "+kredit[i]);
+            }
+        }
+        return "terimakasih  ";
+    }
+
+    /*
+    FUNCTION PEMBAYARAN HARRIER
+     */
+    static String pembayaranHarrier(){
+        Scanner input =new Scanner(System.in);
+        System.out.print("silahkan pilih metode pembayaran (cash/kredit) : ");
+        String bayar = input.nextLine();
+        if (bayar.equals("cash")){
+            var price = 580_000_000;
+            System.out.println("nominal yang anda harus bayar : "+price);
+            System.out.print("KETIK DEAL UNTUK MEMBELI NO UNTUK KEMBALI : ");
+            String beli = input.nextLine();
+            if (beli.equals("DEAL")){
+                System.out.println("Selamat anda telah membeli mobil Harrier");
+            } else if (beli.equals("deal")) {
+                System.out.println("Selamat anda telah membeli mobil Harrier");
+            } else if (beli.equals("NO")) {
+                Toyota();
+            } else if (beli.equals("no")) {
+                Toyota();
+            } else {
+                System.out.println("terimakasih telah mengunjungi Showroom kami");
+            }
+
+        } else if (bayar.equals("kredit")) {
+            String[] kredit = {
+                    "6 bulan",
+                    "12 bulan",
+                    "15 bulan",
+                    "20 bulan",
+                    "30 bulan"
+
+            };
+            System.out.println("==== berikut adalah rincian kredit ====");
+            for (var i = 0; i < kredit.length; i++) {
+                System.out.println((i+1)+". "+kredit[i]);
+            }
+        }
+        return "terimakasih  ";
+    }
+
+    /*
     DI BAWAH INI FUNCTION UNTUK BRAND MOBILNYA
 
     Yang pertama adalah Brand Toyota
@@ -246,7 +396,7 @@ FUNCTION PEMBAYARAN RAIZE
         Scanner input = new Scanner(System.in);
         System.out.print("masukan kisaran harga mobil yang diinginkan : ");
         int inputHarga = input.nextInt();
-        if(inputHarga<200_000_000){
+        if(inputHarga<=200_000_000){
             System.out.println("===== BERIKUT MOBIL YANG TERSEDIA UNTUK KISARAN HARGA YANG ANDA INGINKAN =====");
             String[] daftarMobil = {
                     "Avanza","Agya","Ayla"
@@ -269,7 +419,7 @@ FUNCTION PEMBAYARAN RAIZE
             }
 
 
-        } else if (inputHarga>200_000_000) {
+        } else if (inputHarga>=200_000_000 && inputHarga<=500_000_000) {
             System.out.println("==== BERIKUT MOBIL YANG TERSEDIA UNTUK KISARAN HARGA YANG ANDA INGINKAN ==== ");
             String[] daftarMobil = {
                     "Rush","Freed","Raize"
@@ -288,7 +438,27 @@ FUNCTION PEMBAYARAN RAIZE
             }else {
                 System.out.println("!!!! MOBIL TIDAK TERSEDIA !!!!");
             }
+        } else if (inputHarga<=500_000_000 && inputHarga<=700_000_000 ) {
+            System.out.println("==== BERIKUT MOBIL YANG TERSEDIA UNTUK KISARAN HARGA YANG ANDA INGINKAN ==== ");
+            String[] daftarMobil = {
+                    "Fortuner","Voxy","Harrier"
+            };
+            for (var i = 0;i< daftarMobil.length;i++){
+                System.out.println((i+1)+". "+daftarMobil[i]);
+            }
+            System.out.print("SILAHKAN PILIH MOBIL YANG ANDA MAU : ");
+            int choseMobil =input.nextInt();
+            if (choseMobil == 1){
+                pembayaranFortuner();
+            } else if (choseMobil == 2) {
+                pembayaranVoxy();
+            } else if (choseMobil == 3) {
+                pembayaranHarrier();
+            }else {
+                System.out.println("!!!! MOBIL TIDAK TERSEDIA !!!!");
+            }
         }
+
     }
 
     /*
